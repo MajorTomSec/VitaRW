d51cdf8fbde8c5ba68974a2504acb9de81c80b18
gnmmarechal <mliberato@gs2012.xyz>
2018-03-14 22:21:35 +0000

    Removed all RW mounts but vs0

--- main.c
+++ main.c
@@ -86,20 +86,19 @@ int WriteFile(char *file, void *buf, int size) {
 
 
 int main(int argc, char *argv[]) {
-	int i;
 	void *buf = malloc(0x100);
 
 	psvDebugScreenInit();
     
-    // Mount the partitions as RW
-	for (i = 0; i < 15; i++) {
-		printf("Unmounting partition with id 0x%X\n", (i * 0x100));
-		vshIoUmount(i * 0x100, 0, 0, 0); // id, unk1, unk2, unk3 (flags ?)
-
-		printf("Mounting partition 0x%X with RW permissions...\n", (i * 0x100));
-		_vshIoMount(i * 0x100, 0, 2, buf); // id, unk, permission, work_buffer
-	}
-
+    // Mount vs0 as RW
+	
+	printf("Unmounting partition with id 0x%X\n", 0x300); // 0x300 is vs0
+	vshIoUmount(0x300, 0, 0, 0);
+	
+	printf("Mounting partition 0x%X with RW permissions...\n", 0x300);
+	_vshIoMount(0x300, 0, 2, buf);
+	
+	
     // Copy VitaShell's eboot.bin to vs0:app/NPXS10000/eboot.bin
     
     // Backup Near's eboot elsewhere
